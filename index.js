47dd9faff4fe68b03ace32cf20e4d9398495e1ef
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-07-21 12:04:11 -0700

    Second problem, test for filter(arr, callback)

--- index.js
+++ index.js
@@ -12,4 +12,3 @@ export const map = (array, modification) => {
   return newArray;
 };
 
-export default map;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-import map from '../index.js';
+import { map } from '../index.js';
 
 describe('map', () => {
   it('replaces .map with a .length function', () => {
@@ -8,5 +8,16 @@ describe('map', () => {
 
     expect(actual).toEqual([2, 4, 6, 8, 10]);
     expect(numbers).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+describe('filter', () => {
+  it('takes an array and callback of signature and creates a new array with all items whose callback returned true', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const modification = (item) => item > 3;
+    const expected = [4, 5];
+    const actual = filter(numbers, modification);
+
+    expect(expected).toEqual(actual);
   });
 });
